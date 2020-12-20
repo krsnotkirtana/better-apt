2b5edc12b79c07d66b7497b27a7e6be21ff5a24f
Kartikaye Rishi <kartikayerishi@gmail.com>
2020-12-20 13:11:08 +0530

    added more software

--- bapt.sh
+++ bapt.sh
@@ -33,4 +33,34 @@ then
         sudo apt install synaptic
         echo "Installed Synaptic Package Manager."
     fi
+    if [ $2 = "kdenlive" ]
+    then
+        sudo apt install synaptic
+        echo "Installed kdenlive."
+    fi
+    if [ $2 = "inkscape" ]
+    then
+        sudo apt install inkscape
+        echo "Installed Inkscape."
+    fi
+    if [ $2 = "filezilla" ]
+    then
+        sudo apt install filezilla
+        echo "Installed filezilla."
+    fi
+    if [ $2 = "bleachbit" ]
+    then
+        sudo apt install bleachbit
+        echo "Installed Bleachbit."
+    fi
+    if [ $2 = "audacity" ]
+    then
+        sudo apt install audacity
+        echo "Installed Audacity."
+    fi
+    if [ $2 = "krita" ]
+    then
+        sudo apt install krita
+        echo "Installed Krita."
+    fi
 fi
